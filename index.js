91fbf181878d77041aeba632b85ff7c7d1a30574
Moeka <moeka.k0729@gmail.com>
2024-03-24 20:31:15 +0900

    editing

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const generateImgSrcArray = (targetImg)=>{
-	return targetImg.split("/").pop().replace(".png", "").split("_");
+const generateImgSrcArray = (targetImg, extension)=>{
+	return targetImg.split("/").pop().replace(`.${extension}`, "").split("_");
 }
 
-const changeImgSrcForTarget = (targetImg) =>{
+const changeImgSrcForTarget = (targetImg, extension) =>{
 	let replacing_img = "";
 	let array 		= generateImgSrcArray(targetImg)
 
-	if(!array[array.length -1].includes("c")){
-		array.push("c")
-		replacing_img = `${array.join("_")}.png`
+	if(!array[array.length -1].includes("after")){
+		array.push("after")
+		replacing_img = `${array.join("_")}.${extension}`
 	}
 
 	return replacing_img
 }
-const changeImgSrcForTargetWithMuliti = (targetImg, clickCount, count) =>{
+const changeImgSrcForTargetWithMuliti = (targetImg, clickCount, count, extension) =>{
 	let array = generateImgSrcArray(targetImg)
 
-	if(!array[array.length -1].includes("c")){
+	if(!array[array.length -1].includes("after")){
 		if(clickCount["count"] < count){
-			array.push("c")
+			array.push("after")
 			clickCount["count"] ++
 		}
 		
@@ -27,24 +27,24 @@ const changeImgSrcForTargetWithMuliti = (targetImg, clickCount, count) =>{
 
 		clickCount["count"] --
 	}
-	return `${array.join("_")}.png`
+	return `${array.join("_")}.${extension}`
 }
 
 
-const changeImgSrcForOthers = (targetImg, others) =>{
+const changeImgSrcForOthers = (targetImg, others, extension) =>{
 	let replacing_img = "";
 	let array 		= generateImgSrcArray(targetImg)
 	let array_others	= generateImgSrcArray(others)
 
-	if(array[array.length -1].includes("c")){
-		if(array_others[array_others.length -1].includes("c")){
+	if(array[array.length -1].includes("after")){
+		if(array_others[array_others.length -1].includes("after")){
 			array_others.splice(array_others.length -1, 1)
-			replacing_img = `${array_others.join("_")}.png`
+			replacing_img = `${array_others.join("_")}.${extension}`
 		}
 	}else{
-		if(!array[array.length -1].includes("c")){
-			array_others.push("c")
-			replacing_img = `${array_others.join("_")}.png`
+		if(!array[array.length -1].includes("after")){
+			array_others.push("after")
+			replacing_img = `${array_others.join("_")}.${extension}`
 		}
 	}
 
@@ -52,7 +52,7 @@ const changeImgSrcForOthers = (targetImg, others) =>{
 }
 
 
-const changeAnswerImg = (answers, multi, count = "")=>{
+const changeAnswerImg = (answers, multi, count = "", extension)=>{
 	let clickCount = {count: 0};
 
 	console.log(clickCount);
@@ -63,21 +63,18 @@ const changeAnswerImg = (answers, multi, count = "")=>{
 			let otherImgs = Array.from(answers).filter((answer) => answer !== e.target)
 
 			if(!multi){
-				if(changeImgSrcForTarget(targetImg) !== ""){
-					e.target.src= `img/${changeImgSrcForTarget(targetImg)}`
+				if(changeImgSrcForTarget(targetImg, extension) !== ""){
+					e.target.src= `img/${changeImgSrcForTarget(targetImg, extension)}`
 				}
 				Array.from(otherImgs).forEach((otherImg)=>{
 					if(otherImgs !== e.target){
-						if(changeImgSrcForTarget(targetImg) !== ""){
-							e.target.src= `img/${changeImgSrcForTarget(targetImg)}`
-						}
-						if(changeImgSrcForOthers(e.target.src, otherImg.src) !== ""){
-							otherImg.src= `img/${changeImgSrcForOthers(e.target.src, otherImg.src)}`
+						if(changeImgSrcForOthers(e.target.src, otherImg.src, extension) !== ""){
+							otherImg.src= `img/${changeImgSrcForOthers(e.target.src, otherImg.src, extension)}`
 						}
 					}
 				})
 			}else{
-				e.target.src= `img/${changeImgSrcForTargetWithMuliti(targetImg, clickCount, count)}`
+				e.target.src= `img/${changeImgSrcForTargetWithMuliti(targetImg, clickCount, count, extension)}`
 			}
 		})
 	})
